31e805a33f7eb1b78cb0f98baaf196d2cced1eb6
Tomás Proaño <67541868+tomasproanop@users.noreply.github.com>
2023-08-09 00:16:47 +0200

    Fix responsive nav bar (Bootstrap)

--- script/script.js
+++ script/script.js
@@ -10,22 +10,3 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
-
- // Dark mode button
-const btn = document.querySelector('.btn-toggle');
-
-// Listen for a click on the button
-btn.addEventListener('click', function() {
-  // Then toggle (add/remove) the .dark-theme class to the body
-  document.body.classList.toggle('dark-theme');  
-})
-
-// //responsive navigation bar 
-// function myFunction() {
-//     var x = document.getElementById("myLinks");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   } 
