becc5835400b7502a7dfd375ab87ccedfa28f153
Tomás Proaño <67541868+tomasproanop@users.noreply.github.com>
2023-08-08 14:22:19 +0200

    fix dark nav bar condition

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
     var top = window.scrollY;
-    if(top >=80) {
+    if(top >=770) {
         header.classList.add('navbarDark');
     }
     else {
@@ -19,13 +19,13 @@ btn.addEventListener('click', function() {
   // Then toggle (add/remove) the .dark-theme class to the body
   document.body.classList.toggle('dark-theme');  
 })
-ß
-//responsive navigation bar 
-function myFunction() {
-    var x = document.getElementById("myNavbar");
-    if (x.className === "navbarDark") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbarDark";
-    }
-  } 
+
+// //responsive navigation bar 
+// function myFunction() {
+//     var x = document.getElementById("myLinks");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   } 
